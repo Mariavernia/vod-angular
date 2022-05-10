536e19398ba2627c679f92b4352332b7f25e623a
Mariavernia <maversa2@alumni.uv.es>
2022-05-10 17:25:34 +0200

    Fix error

--- src/app/users/login-dialog.component.ts
+++ src/app/users/login-dialog.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {faEnvelope} from '@fortawesome/free-regular-svg-icons'
+
 @Component({
   templateUrl: 'login-dialog.component.html',
   styleUrls: ['user-dialog.component.css']
